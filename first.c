2587f45e97036a80a467aec61e7e141f363f9f80
sharugowda-123 <sharanya.s@s.amity.edu>
2025-02-12 14:31:53 +0530

    multiply two numbers

--- first.c
+++ first.c
@@ -2,7 +2,7 @@
 int main(){
 	int a=4,b=8;
       
-    I
-	    printf("difference of two numbers is: a-b");
-	    return 0;
+    I       printf("addition of two numbers is:%d , a+b);
+	    printf("difference of two numbers is:%d , a-b");
+	    printf("multiplication of two numbers is:%d , a*b);
 }
